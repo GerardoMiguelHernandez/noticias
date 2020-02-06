43e91c6bcc58d7327e3cf1da3e913502b847dceb
GerardoMiguelHernandez <gerardo.miguel.h@gmail.com>
2020-02-06 08:16:25 -0600

    componentes

---
+++ src/app/components/noticia/noticia.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noticia',
+  templateUrl: './noticia.component.html',
+  styleUrls: ['./noticia.component.scss'],
+})
+export class NoticiaComponent implements OnInit {
+
+
+  ngOnInit() {}
+
+}
